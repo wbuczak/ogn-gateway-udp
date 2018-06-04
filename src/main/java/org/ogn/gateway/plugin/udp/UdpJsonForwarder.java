293c25c10b49c831a1c6c9dd921e210696597019
buczakwk <buczakwk@skysoft-atm.com>
2018-06-04 17:34:12 +0200

    first implementation

---
+++ src/main/java/org/ogn/gateway/plugin/udp/UdpJsonForwarder.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2018 OGN, All Rights Reserved.
+ */
+
+package org.ogn.gateway.plugin.udp;
+
+import java.util.Optional;
+
+import org.ogn.commons.beacon.AircraftBeacon;
+import org.ogn.commons.beacon.AircraftBeaconWithDescriptor;
+import org.ogn.commons.beacon.AircraftDescriptor;
+import org.ogn.commons.beacon.forwarder.OgnAircraftBeaconForwarder;
+import org.ogn.commons.udp.MulticastPublisher;
+import org.ogn.commons.utils.JsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * JSON UDP (multicast) publisher plug-in for OGN gateway
+ * 
+ * @author wbuczak
+ */
+public class UdpJsonForwarder implements OgnAircraftBeaconForwarder {
+
+	private static final Logger				LOG			= LoggerFactory.getLogger(UdpJsonForwarder.class);
+
+	private static final String				VERSION		= "0.0.1";
+
+	private static final Object				syncMonitor	= 1;
+
+	private static volatile boolean			initialized	= false;
+
+	private MulticastPublisher				publisher;
+
+	private ClassPathXmlApplicationContext	ctx;
+
+	private Config							config;
+
+	@Component
+	private static class Config {
+		@Value("${ogn.gateway.plugin.udp.multicast_group:#{systemProperties['OGN_GATEWAY_PLUGIN_UDP_MULTICAST_GROUP']}}")
+		String	multicastGroup;
+
+		@Value("${ogn.gateway.udp.multicast_port:#{systemProperties['OGN_GATEWAY_PLUGIN_UDP_MULTICAST_PORT'] ?: 0}}")
+		int		multicastPort;
+
+		@Value("${ogn.gateway.udp.multicast_ttl:#{systemProperties['OGN_GATEWAY_PLUGIN_UDP_MULTICAST_TTL'] ?: 1}}")
+		int		ttl;
+
+	}
+
+	@Override
+	public void init() {
+
+		synchronized (syncMonitor) {
+			if (!initialized) {
+				try {
+					ctx = new ClassPathXmlApplicationContext("classpath:udpforwarder-application-context.xml");
+					ctx.getEnvironment().setDefaultProfiles("PRO");
+					config = ctx.getBean(Config.class);
+					initialized = true;
+				} catch (final Exception ex) {
+					LOG.error("context initialization failed", ex);
+				}
+
+				publisher = new MulticastPublisher(config.ttl);
+			}
+		} // sync
+
+	}
+
+	@Override
+	public String getName() {
+		return "JSON udp (multicast) publisher";
+	}
+
+	@Override
+	public String getVersion() {
+		return VERSION;
+	}
+
+	@Override
+	public String getDescription() {
+		return "publishes OGN beacons in JSON format (UDP multicast))";
+	}
+
+	@Override
+	public void stop() {
+		publisher.stop();
+		if (ctx != null)
+			ctx.close();
+	}
+
+	@Override
+	public void onBeacon(AircraftBeacon beacon, Optional<AircraftDescriptor> descriptor) {
+		send(new AircraftBeaconWithDescriptor(beacon, descriptor));
+	}
+
+	private void send(AircraftBeaconWithDescriptor beacon) {
+		final String jsonMsg = JsonUtils.toJson(beacon);
+		LOG.trace("sending beacon: {}", jsonMsg);
+		publisher.send(config.multicastGroup, config.multicastPort, jsonMsg.getBytes());
+	}
+
+}
---
+++ src/test/java/org/ogn/gateway/plugin/udp/UdpJsonForwarderTest.java
@@ -0,0 +1,108 @@
+package org.ogn.gateway.plugin.udp;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.ogn.commons.beacon.AircraftBeaconWithDescriptor;
+import org.ogn.commons.utils.JsonUtils;
+
+public class UdpJsonForwarderTest {
+
+	private static final String	MULTICAST_GROUP	= "230.0.0.0";
+	private static final String	MULTICAST_PORT	= "4446";
+
+	static {
+		System.setProperty("ogn.gateway.plugin.udp.multicast_group", MULTICAST_GROUP);
+		System.setProperty("ogn.gateway.udp.multicast_port", MULTICAST_PORT);
+	}
+
+	UdpJsonForwarder	forwarder;
+
+	MulticastReceiver	r1, r2;
+
+	AtomicInteger		counter	= new AtomicInteger();
+
+	static class MulticastReceiver extends Thread {
+		protected MulticastSocket	socket	= null;
+		protected byte[]			buf		= new byte[256];
+		private final AtomicInteger	counter;
+
+		public MulticastReceiver(AtomicInteger counter) {
+			this.counter = counter;
+		}
+
+		@Override
+		public void run() {
+
+			try {
+				final InetAddress group = InetAddress.getByName(MULTICAST_GROUP);
+
+				socket = new MulticastSocket(4446);
+				socket.joinGroup(group);
+
+				while (!Thread.currentThread().isInterrupted()) {
+					final DatagramPacket packet = new DatagramPacket(buf, buf.length);
+					socket.receive(packet);
+					final String received = new String(packet.getData(), 0, packet.getLength());
+					// System.out.println(received);
+
+					final AircraftBeaconWithDescriptor beacon =
+							JsonUtils.fromJson(received, AircraftBeaconWithDescriptor.class);
+					if (beacon != null)
+						counter.incrementAndGet();
+				}
+				socket.leaveGroup(group);
+			} catch (final Exception ex) {
+				// ex.printStackTrace();
+			} finally {
+				socket.close();
+			}
+
+		}
+
+		@Override
+		public void interrupt() {
+			super.interrupt();
+			this.socket.close();
+		}
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		forwarder = new UdpJsonForwarder();
+		forwarder.init();
+
+		r1 = new MulticastReceiver(counter);
+		r2 = new MulticastReceiver(counter);
+		r1.start();
+		r2.start();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		forwarder.stop();
+		r1.interrupt();
+		r2.interrupt();
+	}
+
+	@Test
+	public void test() throws Exception {
+		for (int i = 0; i < 5; i++)
+			forwarder.onBeacon(null, null);
+
+		await().atMost(5, SECONDS).until(() -> {
+			return counter.get() == 10;
+		});
+
+		// Thread.sleep(2000);
+	}
+
+}
